6aaa8c368fb12ca950ea19a277afffd1722126a0
jedsrek89 <marcinj3@gmail.com>
2021-12-06 10:54:03 +0100

    Kalkulator v0.0

---
+++ calculator.js
@@ -0,0 +1,16 @@
+// Napisz funkcję, która zwraca ciąg "something" połączony spracją " "
+// i podany argument a.
+
+
+// document.getElementById("btn05").addEventListener("click", function()
+// {
+//     document.getElementById("poleTekstowe").textContent="jeden";
+//     document.getElementById("poleTekstowe").style.fontWeight="normal";
+// })
+
+// document.getElementById("btn06").addEventListener("click", function()
+// {
+    
+//     document.getElementById("poleTekstowe").textContent="dwa";
+//     document.getElementById("poleTekstowe").style.fontWeight="bold";
+// })
